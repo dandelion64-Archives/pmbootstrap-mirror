92b2e3aa7a09330db7a37aeb1db84c8db45d9558
Caleb Connolly <caleb@postmarketos.org>
2024-05-24 17:58:57 +0200

    config: add get() method
    Cache the config globally and add a method to get it, this allows
    reading the config in places where args isn't available, and avoids
    loading it from disk every time.
    
    Signed-off-by: Caleb Connolly <caleb@postmarketos.org>

--- pmb/config/load.py
+++ pmb/config/load.py
@@ -7,6 +7,8 @@ import sys
 import pmb.config
 from pmb.core.types import PmbArgs
 
+__cfg: configparser.ConfigParser
+
 
 def sanity_check(args: PmbArgs, cfg, key, allowed, print_path):
     value = cfg["pmbootstrap"][key]
@@ -28,7 +30,17 @@ def sanity_checks(args: PmbArgs, cfg, print_path=True):
         sanity_check(args, cfg, key, allowed, print_path)
 
 
+def get(k: str):
+    global __cfg
+    if "__cfg" in globals() and __cfg is not None:
+        return __cfg["pmbootstrap"][k]
+
+    raise RuntimeError("Config not loaded yet")
+
+
 def load(args: PmbArgs):
+    global __cfg
+
     cfg = configparser.ConfigParser()
     if os.path.isfile(args.config):
         cfg.read(args.config)
@@ -55,4 +67,16 @@ def load(args: PmbArgs):
 
     sanity_checks(args, cfg)
 
+    __cfg = cfg
     return cfg
+
+def save(args: PmbArgs, cfg):
+    global __cfg
+    logging.debug(f"Save config: {args.config}")
+    os.makedirs(os.path.dirname(args.config), 0o700, True)
+    with open(args.config, "w") as handle:
+        cfg.write(handle)
+
+    # FIXME: bad bad bad bad bad
+    __cfg = cfg
+
